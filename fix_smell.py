4221cbc879254153f75baf207be2a07e17a9bd90
Ivo Galic <galic1987@gmail.com>
2026-02-27 00:39:08 -0500

    chore: remove untracked build scripts and docs from tree

--- fix_smell.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('src/cognitive/knowledge_graph.rs', 'r') as f:
-    content = f.read()
-
-content = content.replace('smell,\n            entity_id: entity_id.into(),\n            file,\n            line,\n            description: format!("{} detected", smell.clone()),', 'smell: smell.clone(),\n            entity_id: entity_id.into(),\n            file,\n            line,\n            description: format!("{} detected", smell),')
-
-with open('src/cognitive/knowledge_graph.rs', 'w') as f:
-    f.write(content)
